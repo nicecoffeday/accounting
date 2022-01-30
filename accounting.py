ecbb08020b34afd99b1ec977c11dce8cf7d891fc
nicecoffeday <71484908+nicecoffeday@users.noreply.github.com>
2022-01-30 11:53:50 +0800

    Add files via upload
    first

---
+++ accounting.py
@@ -0,0 +1,15 @@
+products = []
+while True:
+    name = input('請輸入商品名稱:')
+    if name == 'q':
+        break
+    price = input('請輸入商品價格:')
+    products.append([name], [price])
+print(products)
+
+for p in profucts:
+    print(p[0], '的價格是', p[1]) 
+
+with open('products.csv', 'w') as f:
+    for p in products:
+        f.write(p[0] + ',' + p[1] +'\n')
