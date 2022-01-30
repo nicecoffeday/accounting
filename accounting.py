b722ffa880c1527c0dff537daafe4e39ee41d09e
nicecoffeday <71484908+nicecoffeday@users.noreply.github.com>
2022-01-30 22:55:40 +0800

    Add files via upload
    refactor

--- accounting.py
+++ accounting.py
@@ -1,34 +1,53 @@
 #讀取檔案
 
-products = []
-with open('products.csv', 'r') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append9([name, price])
-print(products)
+import os 
 
-
+def read_file(filename):
+    products = []
+    with open(filename, 'r') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    return products
 
 #輸入資料
+def user_input(products):
 
-
-while True:
-    name = input('請輸入商品名稱:')
-    if name == 'q':
-        break
-    price = input('請輸入商品價格:')
-    price = int(price)
-    products.append([name, price])
-print(products)
-
+    while True:
+        name = input('請輸入商品名稱:')
+        if name == 'q':
+            break
+        price = input('請輸入商品價格:')
+        price = int(price)
+        products.append([name, price])
+    print(products)
+    return products
 
 # 印出資料
-for p in products:
-    print(p[0], '的價格是', p[1]) 
-
-with open('products.csv', 'w') as f:
-    f.write('商品,價格\n')
+def print_products(products):
     for p in products:
-        f.write(p[0] + ',' + str(p[1]) +'\n') 
+        print(p[0], '的價格是', p[1]) 
+
+#寫入檔案        
+def write_file(filename, products):
+    with open('filename', 'w') as f:
+        f.write('商品,價格\n')
+        for p in products:
+            f.write(p[0] + ',' + str(p[1]) +'\n')
+
+
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename):
+        print('yeah! 找到檔案')
+        products = read_file(filename)
+    else:
+        print('找不到檔案')
+
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv', products) 
+
+main()
